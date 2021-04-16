66da0d9ce9458922ca61ba3a9694dc9a26002bf3
Kha Nguyen <knguyen93@myseneca.ca>
2021-04-16 16:49:49 -0400

    amend Header to use Bootstrap

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import {Container} from "react-bootstrap";
+import {Container, Nav, Navbar, NavbarBrand, NavDropdown} from 'react-bootstrap';
 import {SearchInput} from "evergreen-ui";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
@@ -8,66 +8,37 @@ import './Header.css';
 
 export default function Header(){
 
-    return (     
-        <Container>
-            
-            <div id="header-container">      
-                <div className="header-wrapper">
-                    <nav>
-                        <ul>
-                            <li className="header-menu-list">
-                                <Link to="/">
-                                    <img src="https://www.vudu.com/content/images/logo.svg" alt="Vudu logo" className="home-logo" />
-                                </Link>
-                            </li>
-                            <li className="header-menu-list">                    
-                                    <a href="/movie-list"><span className="text-warning">Movies</span></a>                           
-                            </li>
-                            <li className="header-menu-list">               
-                                    <a href="/tv-list">TV</a>    
-                            </li>
-                            <li className="header-menu-list">                
-                                    <a href="">Free</a>                   
-                            </li>
-                            <li className="header-menu-list">                  
-                                    <a href="">My Vudu</a>                    
-                            </li>
-                        </ul>                               
-                    </nav>
+    return (             
+                
+            <div className="header-container">
+                <Container>
+                    <Navbar variant="dark">
+                        <NavbarBrand>VUDU</NavbarBrand>
+                        <Nav >
+                            <Nav.Link as={Link} to="#">Movies</Nav.Link>
+                            <Nav.Link as={Link} to="#">TV</Nav.Link>                              
+                        </Nav>
+                        <Navbar.Toggle/>
 
-
-                    <div className="header-menu-list">
-                        <Link to="">    
+                        <Navbar.Collapse className="justify-content-end">
+                        <Nav>
+                            <NavDropdown title="Redeem" id="basic-nav-dropdown">
+                                <NavDropdown.Item as={Link} to="#">Digital Copy</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="#">VUDU Code</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Sign In" id="basic-nav-dropdown">
+                                <NavDropdown.Item as={Link} to="#">Sign-In</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="#">Create Account</NavDropdown.Item>
+                            </NavDropdown>
+                            
+                        </Nav>
+                        <Nav>
                             <SearchInput placeholder="Search Movie" width="100%"/>
-                        </Link>
-                    </div>
-
-                    <nav>
-                        <ul>
-                            <li className="header-menu-list list-right">
-                                <Link to="">
-                                    <ShoppingCartIcon style={{fontSize: 25}} />
-                                    <span>2</span>
-                                </Link>
-
-                            </li>
-                            <li className="header-menu-list list-right">
-                                <div className="dropdown">                                    
-                                    <a className="dropbtn" href="/sign-in">
-                                        Sign In
-                                    </a>                                       
-                                    <div className="dropdown-content">
-                                        <a href="/sign-in">Sign In</a>
-                                        <a href="/account-setup">Create Account</a>
-                                    </div>
-                                </div>  
-                            </li>
-                        </ul>
-                    </nav>                    
-
-                </div>   
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Container>
             </div>
-        </Container>
 
     
     )
